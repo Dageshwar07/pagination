20684ca4314945040010e617771915bd5fbddd0f
Dageshwar Das <dageshwardasmanikpuri996@gmail.com>
2023-09-29 12:20:30 +0530

    hii

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Headers from './components/Header';
+import Home from './components/Home';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <>
+      <Headers />
+      <Home />
+    </>
+  );
+}
+
+export default App;
